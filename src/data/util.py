5695735c66c2eebf2d4f008f2910e8d9de2bb91e
gviglione <g.viglione4@studenti.unisannio.it>
2025-05-27 19:43:40 +0200

    Added data understanding and parsing

---
+++ src/data/util.py
@@ -0,0 +1,15 @@
+import os.path
+
+import pandas as pd
+from base import DATA_DIR
+
+def save_to_csv(data, fname):
+    data.to_csv(os.path.join(DATA_DIR, fname), index=False)
+def load_from_excel(fname, **kwargs):
+    return pd.read_excel(os.path.join(DATA_DIR, fname), **kwargs)
+def load_from_csv(fname, **kwargs):
+    return pd.read_csv(filepath_or_buffer=os.path.join(DATA_DIR, fname), **kwargs)
+def apply_and_save(df: pd.DataFrame, func, fname, **kwargs):
+    new_df = func(df.copy(deep=True), **kwargs)
+    new_df.to_csv(os.path.join(DATA_DIR, fname), index=False)
+    return new_df
